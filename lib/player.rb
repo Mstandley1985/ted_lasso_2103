9ff8ce878bd07e0dcce3caa8e239bb8a5748b3cb
Mitch Standley <mitch_standley@yahoo.com>
2021-04-07 10:16:14 -0500

    Initial commit

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+
+  attr_reader :name,
+              :position,
+              :salary
+  def initialize(info)
+    @name = info[:name]
+    @position = info[:position]
+    @salary = info[:salary]
+  end
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/player'
+
+describe Player do
+
+  it 'exists' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
+
+    expect(roy).to be_instance_of(Player)
+  end
+
+  it 'player has attributes' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
+
+    expect(roy.name).to eq("Roy Kent")
+    # expect(roy.position).to eq("Center Midfielder")
+    # expect(roy.salary).to eq(1_000_000)
+  end
+end
