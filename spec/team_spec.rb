7ceef35e18d8bb684c672d19b71d0277f33a7bec
Mitch Standley <mitch_standley@yahoo.com>
2021-04-07 10:46:09 -0500

    Add coach test with method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -20,5 +20,21 @@ describe Team do
     expect(richmond.name).to eq("AFC Richmond")
   end
 
+  it 'team has a coach' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond.coach).to eq("Ted Lasso")
+  end
+
+  it 'team has players' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond.players).to eq([roy, sam])
+  end
+
 
 end
