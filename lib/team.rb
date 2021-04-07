8f7c9dddd6d2a94604122b0d76d8416068025a99
Mitch Standley <mitch_standley@yahoo.com>
2021-04-07 10:42:05 -0500

    Add name and exist tests with working code

--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,13 @@
 class Team
-  
+
+  attr_reader :name,
+              :coach,
+              :players
+  def initialize(name, coach, players)
+    @name    = name
+    @coach   = coach
+    @players = []
+  end
 
 
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,9 +5,19 @@ require './lib/team'
 describe Team do
 
   it 'exists' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
     richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
 
     expect(richmond).to be_instance_of(Team)
+  end
+
+  it 'team has a name' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond.name).to eq("AFC Richmond")
   end
 
 
